79d7fe55ecb377875996d7f235b9a868052a7b33
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-04-18 19:12:38 -0300

    requisito 3 - criando header e recuperando nome de usuario cadastrado

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
           <Route path="/favorites" component={ Favorites } />
           <Route path="/profile" component={ Profile } />
           <Route path="/profile/edit" component={ ProfileEdit } />
-          <Route path="/" component={ Login } />
+          <Route exact path="/" component={ Login } />
           <Route component={ NotFound } />
         </Switch>
       </BrowserRouter>
---
+++ src/Header.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { getUser } from './services/userAPI';
+import Loading from './pages/Loading';
+
+export default class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: '',
+      username: '',
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      loading: true,
+
+    }, async () => {
+      const user = await getUser();
+
+      this.setState({
+        loading: false,
+        username: user,
+      });
+    });
+  }
+
+  render() {
+    const { loading, username: { name } } = this.state;
+    return (
+      <header data-testid="header-component">
+
+        {loading === false ? (
+
+          <h1 data-testid="header-user-name">
+            {`Bem-vindo(a) ${name}`}
+          </h1>) : <Loading /> }
+
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  loading: PropTypes.bool,
+}.isRequired;
