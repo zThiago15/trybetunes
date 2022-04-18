568b9f61080cd6a19b012f3186b52724f85877cc
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-04-18 18:03:32 -0300

    requisito 2 - criando form de login para o usuario acessar a pag e mostrando mensagem de carregamento apos criar usuario e antes de redirecionar a pag

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,74 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      loading: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.submitData = this.submitData.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  submitData(event) {
+    event.preventDefault();
+    const { name } = this.state;
+    const { history } = this.props;
+
+    this.setState({
+      loading: true,
+    }, async () => {
+      await createUser({ name });
+      history.push('/search');
+    });
+  }
+
   render() {
+    const { name, loading } = this.state;
+    const minLength = 3;
+
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+
+        {loading && <Loading />}
+        <form onSubmit={ this.submitData }>
+          <label htmlFor="name">
+            Nome:
+            <input
+              name="name"
+              onChange={ this.handleChange }
+              value={ name }
+              data-testid="login-name-input"
+              minLength="3"
+            />
+          </label>
+
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            disabled={ name.length < minLength }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.object,
+}.isRequired;
