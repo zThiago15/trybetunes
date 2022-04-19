a638dcf5cb3ec74fcb6fe152519e21f857416692
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-04-18 23:32:23 -0300

    requisito 4 - criando links de navegacao do search, favorite songs e profile no cabecalho

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { getUser } from './services/userAPI';
 import Loading from './pages/Loading';
+import { Link } from 'react-router-dom';
 
 export default class Header extends Component {
   constructor() {
@@ -30,13 +31,19 @@ export default class Header extends Component {
     const { loading, username: { name } } = this.state;
     return (
       <header data-testid="header-component">
+        <nav className="navHeader">
+          <ul><Link data-testid="link-to-search" to="/search">Search</Link></ul>
+          <ul>
+            <Link data-testid="link-to-favorites" to="/favorites">Favorite Songs</Link>
+          </ul>
+          <ul><Link data-testid="link-to-profile" to="/profile">Profile</Link></ul>
+        </nav>
 
         {loading === false ? (
 
           <h1 data-testid="header-user-name">
             {`Bem-vindo(a) ${name}`}
           </h1>) : <Loading /> }
-
       </header>
     );
   }
