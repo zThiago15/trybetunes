cb1fe31004ece828d0e60e941ee74dd4c00cd861
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-04-18 23:53:40 -0300

    requisito 5 - criando formulario para pesquisar artistas, texto deve ter ao menos 2 caracteres para habilitar o botao

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,36 @@ import React, { Component } from 'react';
 import Header from '../Header';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      iptSearch: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { iptSearch } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            placeholder="Pesquisar nome do artista"
+            name="iptSearch"
+            value={ iptSearch }
+            onChange={ this.handleChange }
+            data-testid="search-artist-input"
+          />
+          <button type="button" disabled={ iptSearch.length < 2 } data-testid="search-artist-button">Procurar</button>
+        </form>
       </div>
     );
   }
