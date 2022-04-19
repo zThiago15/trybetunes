b982cec130656213e7397664eedc7afbc478033d
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-04-19 17:36:26 -0300

    requisito 8 - sistema de adicionar musica a lista de favoritos

--- src/pages/MusicCard.js
+++ src/pages/MusicCard.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 export default class MusicCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isFavorite: false,
+      loading: false,
+    };
+
+    this.favoriteSong = this.favoriteSong.bind(this);
+  }
+
+  favoriteSong({ target }) {
+    const { song } = this.props;
+
+    this.setState({
+      isFavorite: target.checked,
+      loading: true,
+    }, async () => {
+      await addSong(song);
+
+      this.setState({
+        loading: false,
+      });
+    });
+  }
+
   render() {
-    const { trackName, previewUrl } = this.props.song;
+    const { trackName, previewUrl, trackId } = this.props.song;
+    const { loading, isFavorite } = this.state;
 
     return (
       <div>
-        {
-        trackName && previewUrl
+        {!loading
+        && trackName && previewUrl
         && (
           <>
             <p>{ trackName }</p>
@@ -15,9 +43,17 @@ export default class MusicCard extends Component {
               <track kind="captions" />
               <code>audio</code>
             </audio>
-          </>)
-
-        }
+            <label htmlFor="favorite">
+              Favorita
+              <input
+                data-testid={ `checkbox-music-${trackId}` }
+                onChange={ this.favoriteSong }
+                type="checkbox"
+                checked={ isFavorite }
+              />
+            </label>
+          </>)}
+        {loading === true && <Loading />}
 
       </div>
     );
