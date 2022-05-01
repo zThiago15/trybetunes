28c0747c1aabe57baa1004f5eda55086e218084d
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-05-01 17:27:59 -0300

    Estilizando pagina de login com responsividade

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import styles from '../css/Loading.module.css';
 
 export default class Loading extends Component {
   render() {
     return (
-      <div>Carregando...</div>
+      <div className={ styles.loading }>Carregando...</div>
     );
   }
 }
